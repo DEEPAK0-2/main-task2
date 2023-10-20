72af49d12ea7a4cb643c41b69d627db30483c7cc
DEEPAK0-2 <147275541+DEEPAK0-2@users.noreply.github.com>
2023-10-20 16:35:52 +0530

    Add files via upload

---
+++ myaccount.php
@@ -0,0 +1,93 @@
+<?php include('inc/header.php');  ?>
+
+<?php include('inc/nav.php');  ?>
+ 
+ 
+ 
+<div class="container text-white">
+    <h2 class='text-center text-white'>My Account</h2>
+
+    <section id="content">
+		<div class="content-blog content-account">
+			<div class="container">
+				<div class="row">
+				 
+					<div class="col-md-12">
+
+			<h3>Recent Orders</h3>
+			<br>
+			<table class="cart-table account-table table table-bordered bg-white text-dark">
+				<thead>
+					<tr>
+						<th>Order</th>
+						<th>Date</th>
+						<th>Status</th>
+						<th>Total</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+						<td>
+					
+						</td>
+					</tr>
+				</tbody>
+			</table>		
+
+		 
+
+			<div class="ma-address">
+						<h3>My Addresses</h3>
+						<p>The following addresses will be used on the checkout page by default.</p>
+
+			<div class="row  bg-white text-dark px-5 py-3">
+				<div class="col-md-6">
+								<h4>Billing Address <a href="#">Edit</a></h4>
+					<p>
+						DEEPAK KUMAR<br>
+						Vill-BIJNOUR<br>
+						LUCKNOW<br>
+						LUCKNOW<br>
+						226002<br>
+						 
+						Uttar Pradesh
+					</p>
+				</div>
+
+				<div class="col-md-6">
+								<h4>Shipping Address <a href="#">Edit</a></h4>
+					<p>
+						DEEPAK<br>
+						BIJNOUR , LUCKNOW<br>
+						BANGALABAJAR<br>
+						LUCKNOW<br>
+						226012<br>
+						 
+						LUCKNOW
+					</p>
+
+				</div>
+			</div>
+
+
+
+			</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+ 
+</div>
+
+
+
+
+
+
+
+<?php include('inc/footer.php');  ?>
+
+
